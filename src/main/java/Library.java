3fdcb9183ec7a351040dffbf2a1b20767ed0fe9a
petermordaunt <peetfire@hotmail.com>
2022-02-08 16:19:21 +0000

    Library - tests created and passed for startsEmpty, canAddBook and canNotAddBook

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -1,2 +1,43 @@
+import java.util.ArrayList;
+
 public class Library {
+    ArrayList<Book> books;
+    int capacity;
+
+    public Library(int capacity) {
+        this.books = new ArrayList<>();
+        this.capacity = capacity;
+    }
+
+    public int getBookCount() {
+        return this.getBooks().size();
+    }
+
+    public ArrayList<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(ArrayList<Book> books) {
+        this.books = books;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public boolean addBook(Book book) {
+        if(this.hasCapacity()){
+            this.books.add(book);
+            return true;
+        }
+        return false;
+    }
+
+    private boolean hasCapacity() {
+        return this.getBookCount() <= this.getCapacity() - 1;
+    }
 }
---
+++ src/test/java/LibraryTest.java
@@ -0,0 +1,40 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LibraryTest {
+    Library library;
+    Book book1, book2, book3, book4, book5;
+
+    @Before
+    public void before(){
+        library = new Library(5);
+        book1 = new Book("Mog", "Judith Kerr", "Children");
+        book2 = new Book("Fup", "Jim Dodge", "Fiction");
+        book3 = new Book("Oryx and Crake", "Margaret Atwood", "Sci-Fi");
+        book4 = new Book("Burglar Bill", "Janet and Allan Ahlberg", "Children");
+        book5 = new Book("The Tiger Who Came To Tea", "Judith Kerr", "Children");
+    }
+
+    @Test
+    public void startsEmpty(){
+        assertEquals(0, library.getBookCount());
+    }
+
+    @Test
+    public void canAddBook(){
+        assertTrue(library.addBook(book1));
+    }
+
+    @Test
+    public void canNotAddBook(){
+        library.addBook(book1);
+        library.addBook(book2);
+        library.addBook(book3);
+        library.addBook(book4);
+        library.addBook(book5);
+        assertFalse(library.addBook(book1));
+    }
+
+}
